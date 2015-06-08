148127cbeaa7a5814408beb9cb8878dfc44bad5f
Samar Panda <samar.panda84@gmail.com>
2015-06-08 07:59:45 +0530

    Auto Curry functional concepts

--- excercises/0-curry/app.js
+++ excercises/0-curry/app.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+/**
+ * To Array
+ */
+function toArray (args) {
+	return [].slice.call(args);
+}
+
+/**
+ * Curry Function
+ */
+function curry (fn) {
+	var args = Array.prototype.slice.call(arguments, 1);
+	function f () {
+		return fn.apply(this, args.concat(toArray(arguments)));
+	}
+	return f;
+}
---
+++ excercises/1-auto-curry/app.js
@@ -0,0 +1,48 @@
+/**
+ * To Array
+ */
+function toArray (args) {
+	return [].slice.call(args);
+}
+
+/**
+ * Curry Function
+ */
+function curry (fn) {
+	var args = Array.prototype.slice.call(arguments, 1);
+	function f () {
+		return fn.apply(this, args.concat(toArray(arguments)));
+	}
+	return f;
+}
+
+/**
+ * Auto Curry function
+ */
+function autoCurry (fn, numArgs) {
+	numArgs = numArgs || fn.length;
+	function f () {
+		if( arguments.length < numArgs ) {
+			return numArgs - arguments.length > 0 ?
+				autoCurry(curry.apply(this, [fn].concat(toArray(arguments))), numArgs - arguments.length) :
+				curry.apply(this, [fn].concat(toArray(arguments)));
+		} else {
+			return fn.apply(this, arguments);
+		}
+	}
+	f.toString = function () {
+		return fn.toString();
+	};
+	f.curried = true;
+	return f;
+}
+
+
+var add = function(x, y){
+  return x + y;
+};
+
+var addCurry = autoCurry(add);
+var add1 = addCurry(1);
+//console.log(add1);
+console.log(add1(2));
