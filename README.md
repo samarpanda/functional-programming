e58c499abeb65008e191b4e116900380b0ea9a23
Samar Panda <samar.panda84@gmail.com>
2015-06-11 21:43:39 +0530

    Curry and Compose usage updated

--- README.md
+++ README.md
@@ -2,7 +2,75 @@
 
 ### Curry
 
+```js
+/**
+ * Curry function
+ */
+var curry = function(fn){
+	return function(){
+		if(fn.length > arguments.length){
+			var slice = Array.prototype.slice;
+			var args = slice.apply(arguments);
+			return function(){
+				return fn.apply(null, args.concat(slice.apply(arguments)));
+			};
+		}else
+		  return fn.apply(null, arguments);
+	};
+}
+
+/**
+ * Subcurry Function
+ */
+function sub_curry (fn) {
+	var args = Array.prototype.slice.call(arguments, 1);
+	function f () {
+		return fn.apply(this, args.concat(toArray(arguments)));
+	}
+	return f;
+}
+
+/**
+ * To Array
+ */
+function toArray (args) {
+	return [].slice.call(args);
+}
+```
+
 ### Compose
+
+```js
+/**
+ * Compose function
+ */
+function compose(g, f){
+	return function(x){
+		return g(f(x));
+	}
+}
+
+/**
+ * Reverse String
+ */
+function reverse(s){
+	return s.split('').reverse().join('');
+}
+
+/**
+ * Capitalize first Letter
+ */
+function properNoun(s){
+	return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+//Example of compose functionality
+var revCap = compose(reverse, properNoun);
+console.log(revCap("samar"));//"ramaS"
+
+```
+
+Try an example [demo jsbin](http://jsbin.com/watisu/1/edit?js,console)
 
 ### Functor
 
--- excercises/0-curry/app.js
+++ excercises/0-curry/app.js
@@ -6,12 +6,84 @@ function toArray (args) {
 }
 
 /**
- * Curry Function
+ * Subcurry Function
  */
-function curry (fn) {
+function sub_curry (fn) {
 	var args = Array.prototype.slice.call(arguments, 1);
 	function f () {
 		return fn.apply(this, args.concat(toArray(arguments)));
 	}
 	return f;
 }
+
+/**
+ * Curry function
+ */
+var curry = function(fn){
+	return function(){
+		if(fn.length > arguments.length){
+			var slice = Array.prototype.slice;
+			var args = slice.apply(arguments);
+			return function(){
+				return fn.apply(null, args.concat(slice.apply(arguments)));
+			};
+		}else
+		  return fn.apply(null, arguments);
+	};
+}
+
+/**
+ * Sample Array of people
+ */
+var people = [
+	{'n': 'Mark'},
+	{'n': 'Marc'},
+	{'n': 'Rayn'}
+];
+
+/**
+ * Get property of object
+ */
+function get (property, object) {
+	return object[property];
+}
+
+var getCurry = curry(function(property, object){
+  return object[property];
+});
+
+/**
+ * Get PersonName from Object person
+ */
+function getPersonName (person) {
+	return get('n', person);
+}
+
+// var names = people.map(getPersonName);
+var names = people.map(getCurry('n'));
+// console.log(names);
+
+
+var splitFn = function(str, separator){
+  return function(separator){
+    return str.split(separator);
+  }
+}
+
+var splitCurry1 = splitFn.apply(null, ["mississippi"]);
+// console.log(splitCurry1("i"));
+
+var splitCurry2 = splitFn.call(null, "mississippi");
+// console.log(splitCurry2("i"));
+
+var splitCurry3 = curry(splitFn("mississippi"));
+// console.log(splitCurry3("i"));
+
+var fn = function(a, b, c) {
+	return [a, b, c];
+};
+
+// console.log(fn("a", "b", "c"));
+// console.log(sub_curry(fn, "a")("b", "c"));
+console.log(sub_curry(fn, "a", "b")("c"));
+
