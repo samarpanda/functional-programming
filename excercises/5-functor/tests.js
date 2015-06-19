702b91143eb516691dd5a1d858aaf60d05f5420f
Samar Panda <samar.panda84@gmail.com>
2015-06-19 20:28:41 +0530

    updated test

--- excercises/5-functor/tests.js
+++ excercises/5-functor/tests.js
@@ -1,10 +1,14 @@
 var assert = require('../assert');
 var app = require('./app');
 
-exports.run = () => {
+function containerAddOperation(){
 	var c = app.Container(2);
 	var d = c.map(function(x){
 		return x+2;
 	});
 	assert(d.val === 4, "Add operation on Container");
+}
+
+exports.run = () => {
+	containerAddOperation();
 };
