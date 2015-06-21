0a1bc27278737e0e8ee23d1656e03605a8e20b7b
Samar Panda <samar.panda84@gmail.com>
2015-06-21 19:20:26 +0530

    typo error

--- excercises/1-curry/app.js
+++ excercises/1-curry/app.js
@@ -8,6 +8,14 @@ function get (property, object) {
 }
 
 /**
+ * get curried version
+ * Curried version supports delayed version of get
+ */
+var getCurry = curry(function(property, object){
+  return object[property];
+});
+
+/**
  * Get PersonName from Object person
  */
 function getPersonName (person) {
@@ -24,42 +32,8 @@ var splitFn = function(str, separator){
 }
 
 exports.get = get;
+exports.getCurry = getCurry;
 exports.getPersonName = getPersonName;
 exports.splitFn = splitFn;
-
-
-/**
- * Sample Array of people
- */
-var people = [
-	{'n': 'Mark'},
-	{'n': 'Marc'},
-	{'n': 'Rayn'}
-];
-
-var old_names = people.map(getPersonName);
-console.log(old_names);
-
-var getCurry = curry(function(property, object){
-  return object[property];
-});
-
-var getCurry1 = curry(get);
-
-var names = people.map(getCurry('n'));
-console.log(names);
-
-var names1 = people.map(getCurry1('n'));
-console.log(names1);
-
-
-var splitCurry1 = splitFn.apply(null, ["mississippi"]);
-// console.log(splitCurry1("i"));
-
-var splitCurry2 = splitFn.call(null, "mississippi");
-// console.log(splitCurry2("i"));
-
-var splitCurry3 = curry(splitFn("mississippi"));
-// console.log(splitCurry3("i"));
 
 require('./tests.js').run();
--- excercises/1-curry/tests.js
+++ excercises/1-curry/tests.js
@@ -1,8 +1,10 @@
 var assert = require('../assert');
+var curry = require('./curry').curry;
 var app = require('./app');
 
-var getPersonName = app.getPersonName;
 var get = app.get;
+var getCurry = app.getCurry;
+var getPersonName = app.getPersonName;
 var splitFn = app.splitFn;
 
 /**
@@ -14,18 +16,45 @@ var people = [
 	{'n': 'Rayn'}
 ];
 
+var str = "mississippi";
+
 function appRequiredFunctions(app){
 	return typeof app.get === 'function' &&
 		typeof app.getPersonName === 'function' &&
 		typeof app.splitFn === 'function';
 }
 
+/**
+ * Maps works on the person object
+ * Wraps back into and array and returns
+ */
 function showNamesInPeople(){
-	var names = people.map(getPersonName);
-	console.log(names);
+	var names1 = people.map(getPersonName);
+	console.log(names1);
+
+	var names2 = people.map(getCurry('n'));
+	console.log(names2);
+}
+
+function usingApply(){
+	var splitApply = splitFn.apply(null, [str]);
+	console.log("Apply:", splitApply("i"));
+}
+
+function usingCall(){
+	var splitCall = splitFn.call(null, str);
+	console.log("Call:", splitCall("i"));
+}
+
+function usingCurry(){
+	var splitCurry = curry(splitFn(str));
+	console.log("Curry:", splitCurry("i"));
 }
 
 exports.run = () => {
 	assert(appRequiredFunctions(app), 'App has all required functions');
 	showNamesInPeople();
+	usingApply();
+	usingCall();
+	usingCurry();
 };
