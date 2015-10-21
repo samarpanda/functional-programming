80482e7683c2ad5417f0fa957110d1d8cb4a7dc1
Samar Panda <samar.panda84@gmail.com>
2015-10-21 18:04:17 +0530

    Added Maybe Monard

--- excercises/6-monad/app.js
+++ excercises/6-monad/app.js
@@ -8,9 +8,45 @@ var Identity = function(){
 /**
  * Maybe
  */
- var Maybe = function(){
+var Maybe = function(value){
+	var Nothing = {
+		bind: function(fn){
+			return this;
+		},
+		isNothing: function(){
+			return true;
+		},
+		val: function(){
+			throw new Error('Cannot call val of Nothing');
+		},
+		maybe: function(def, fn){
+			return def;
+		}
+	};
 
- };
+	var Something = function(value){
+		return {
+			bind: function(fn){
+				return Maybe(fn.call(this, value));
+			},
+			isNothing: function(){
+				return false;
+			},
+			val: function(){
+				return value;
+			},
+			maybe: function(def, fn){
+				return fn.call(this, value);
+			}
+		};
+	};
+
+	if(typeof value === 'undefined' || value === null){
+		return Nothing;
+	}
+	return Something(value);
+
+};
 
 /**
  * Either
